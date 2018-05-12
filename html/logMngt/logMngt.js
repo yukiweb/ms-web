e7b30796a42c4f005cb7b1b8d09870fccbaa0031
lwx567251 <liuyu109@huawei.com>
2018-05-12 13:19:43 +0800

    修改了logMgnt

--- html/logMngt/logMngt.js
+++ html/logMngt/logMngt.js
@@ -1,82 +1,71 @@
 var mysupport = angular.module("mysupport");
-mysupport.controller("logMngt", ["$scope", "$http", function($scope, $http) {
-        //获取当前时间：$filter('date')(new Date().getTime(),'yyyy-MM-dd')
-        //初始化，右边区域的高度固定
-        $scope.init=function(){
-            $scope.draw();
-        };
-        $scope.draw = function() {
-            var mainHeight = $(window).height() - $('[ui-view=header]').height() - $('[ui-view=footer]').height();
-            $('[ui-view=right]').css('max-height', mainHeight - 15);
-        };
-    $scope.$on('select',function(e,newName){
-        // console.log(newName)
-        var html = ''
-        // for(item in newName){
-        //    html+=newName[item]+"-";
-        // }
-        for(var i = 0;i<newName.length;i++){
-            if(i<2){
-                html+=newName[i]+"-";
-            }else{
-                html+=newName[i];
-            }
-        }
-        $('.myStartDate').val(html);
-    })
-    }
-]);
-mysupport.controller('dateStart',["$scope","$http",function ($scope,$http) {
-    $scope.$on('select',function(e,newName){
-        // console.log(newName)
-        var html = ''
-        // for(item in newName){
-        //    html+=newName[item]+"-";
-        // }
-        for(var i = 0;i<newName.length;i++){
-            if(i<2){
-                html+=newName[i]+"-";
-            }else{
-                html+=newName[i];
-            }
-        }
-        $('.myStartDate').val(html);
-    })
-}]);
-mysupport.controller('dateEnd',["$scope","$http",function ($scope,$http) {
-    $scope.$on('select',function(e,newName){
-        // console.log(newName)
-        var html = ''
-        // for(item in newName){
-        //    html+=newName[item]+"-";
-        // }
-        for(var i = 0;i<newName.length;i++){
-            if(i<2){
-                html+=newName[i]+"-";
-            }else{
-                html+=newName[i];
-            }
-        }
-        $('.myEndDate').val(html);
-    })
-}]);
-//日历时间
-mysupport.directive('datepicker', function(){
-    return {
-        restrict: 'EAC',
-        controller: 'DatePickerCtl',
-        templateUrl: 'datepicker/datepicker.html',
-        scope: {
-            'value': '='
-        },
-        link: function(scope){
-            scope.$watch('value', function(oldVal, newVal){
-                console.log('value = ' + $scope.value);
-            });
-        },
-        controller:function(){
-
-
-        }
+mysupport.controller("logMngt", ["$scope", "$http", function ($scope, $http) {
+    //获取当前时间：$filter('date')(new Date().getTime(),'yyyy-MM-dd')
+    //初始化，右边区域的高度固定
+    $scope.init = function () {
+        $scope.draw();
     };
-});
+    $scope.draw = function () {
+        var mainHeight = $(window).height() - $('[ui-view=header]').height() - $('[ui-view=footer]').height();
+        $('[ui-view=right]').css('max-height', mainHeight - 15);
+    };
+    $http.get('mysupport/sds/findLoggerInfo', {
+        params: {
+            //  "appId": $stateParams.businessId
+            "userId": null,
+            "appId": "app_201804290535310195",//这个值应该是输入框的值
+            "start": "2018-05-01 00:00:01",//这个值应该是输入框的值
+            "end": "2018-05-10 23:59:59"//这个值应该是输入框的值
+        }
+    })
+        .then(function (resp, status, headers, config) {
+            if (resp && resp.data) {
+                $scope.app = resp.data;
+            }
+        }, function (resp) {
+
+        })
+    $scope.isMoveUp = true;
+    $scope.isMoveDown = !$scope.isMoveUp;
+    $scope.isShowDetail = false;
+    $scope.changeMoveClass = function () {
+        $scope.isMoveUp = !$scope.isMoveUp;
+        $scope.isMoveDown = !$scope.isMoveUp;
+        $scope.isShowDetail = !$scope.isShowDetail;
+        console.log($scope.isMoveUp);
+        console.log($scope.isMoveDown);
+    }
+}
+]);
+mysupport.controller('dateStart', ["$scope", "$http", function ($scope, $http) {
+    $scope.$on('select', function (e, newName) {
+        var html = ''
+        for (var i = 0; i < newName.length; i++) {
+            if (i < 2) {
+                html += newName[i] + "-";
+            } else {
+                html += newName[i];
+            }
+        }
+        $scope.starts = html;
+        if (newName.length == 3) {
+            $scope.isShowStart = !$scope.isShowStart;
+        }
+    })
+}]);
+mysupport.controller('dateEnd', ["$scope", "$http", function ($scope, $http) {
+    $scope.$on('select', function (e, newName) {
+        var html = ''
+        for (var i = 0; i < newName.length; i++) {
+            if (i < 2) {
+                html += newName[i] + "-";
+            } else {
+                html += newName[i];
+            }
+        }
+        $scope.ends = html;
+        if (newName.length == 3) {
+            $scope.isShowEnd = !$scope.isShowEnd;
+        }
+    })
+}]);
